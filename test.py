60f805c7535a002f1a278f7371b84b1509445642
chepiga1995 <ura1995ura@gmail.com>
2015-11-29 00:05:39 +0200

    test

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ train = theano.function(inputs=[X,Y], outputs=cost, updates=update, allow_input_
 predict = theano.function(inputs=[X], outputs=pred_y, allow_input_downcast=True)
 
 for x in range(TRAIN_CIRCLES):
-	for start, end in zip(range(0, len(test_img), 100), range(100, len(test_img), 100):
+	for start, end in zip(range(0, len(test_img), 100), range(100, len(test_img), 100)):
 		train(train_img[start:end], train_res[start:end])
 	if x % 10 == 0:
 		print test_accuracy(TEST_SIZE, predict, test_img, test_res)
