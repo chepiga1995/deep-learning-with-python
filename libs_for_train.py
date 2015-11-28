368f476a76e9c15059872f452c378b1c1b51c649
chepiga1995 <ura1995ura@gmail.com>
2015-11-29 01:37:26 +0200

    test

--- libs_for_train.py
+++ libs_for_train.py
@@ -1,16 +1,15 @@
 import numpy as np
 import theano
 import theano.tensor as T
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+
+srng = RandomStreams()
 rng = np.random
 
 def floatX(X):
 	return np.asarray(X, dtype=theano.config.floatX)
 def init_weight(share):
 	return theano.shared(floatX(rng.randn(*share) * 0.01))
-def model(x, w_h, w_o):
-	y = T.nnet.sigmoid(T.dot(x, w_h))
-	return T.nnet.softmax(T.dot(y, w_o))
-
 def test_accuracy(size, predict, test_img, test_res):
 	res = predict(test_img);
 	sum_res = 0 
@@ -19,9 +18,39 @@ def test_accuracy(size, predict, test_img, test_res):
 		sum_res += (res[i] == test_res[i].index(1))
 	return float(sum_res) / size
 
-def sgd(cost, params, speed=0.05):
-	y = T.grad(cost=cost, wrt=params)
-	update = []
-	for g, p in zip(y, params):
-		update.append([p, p - g * speed])
-	return update
+def rectify(X):
+    return T.maximum(X, 0.)
+
+def softmax(X):
+    e_x = T.exp(X - X.max(axis=1).dimshuffle(0, 'x'))
+    return e_x / e_x.sum(axis=1).dimshuffle(0, 'x')
+
+def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
+    grads = T.grad(cost=cost, wrt=params)
+    updates = []
+    for p, g in zip(params, grads):
+        acc = theano.shared(p.get_value() * 0.)
+        acc_new = rho * acc + (1 - rho) * g ** 2
+        gradient_scaling = T.sqrt(acc_new + epsilon)
+        g = g / gradient_scaling
+        updates.append((acc, acc_new))
+        updates.append((p, p - lr * g))
+    return updates
+
+def dropout(X, p=0.):
+    if p > 0:
+        retain_prob = 1 - p
+        X *= srng.binomial(X.shape, p=retain_prob, dtype=theano.config.floatX)
+        X /= retain_prob
+    return X
+
+def model(X, w_h, w_h2, w_o, p_drop_input, p_drop_hidden):
+    X = dropout(X, p_drop_input)
+    h = rectify(T.dot(X, w_h))
+
+    h = dropout(h, p_drop_hidden)
+    h2 = rectify(T.dot(h, w_h2))
+
+    h2 = dropout(h2, p_drop_hidden)
+    py_x = softmax(T.dot(h2, w_o))
+    return h, h2, py_x
--- test.py
+++ test.py
@@ -7,18 +7,20 @@ SPEED = 0.05
 TRAIN_CIRCLES = 130
 BATCHES = 128
 
-
-w_h = init_weight((SIZE * SIZE, 625))
-w_o = init_weight((625, 10))
 X = T.fmatrix('x')
 Y = T.fmatrix('y')
 
-py_x = model(X, w_h, w_o)
-pred_y = T.argmax(py_x, axis=1)
-# print theano.function([X], py_x)(result[0])
+w_h = init_weight((SIZE * SIZE, 625))
+w_h2 = init_weights((625, 625))
+w_o = init_weights((625, 10))
 
-cost = T.mean(T.nnet.categorical_crossentropy(py_x, Y))
-update = sgd(cost, [w_h, w_o], SPEED)
+noise_h, noise_h2, noise_py_x = model(X, w_h, w_h2, w_o, 0.2, 0.5)
+h, h2, py_x = model(X, w_h, w_h2, w_o, 0., 0.)
+y_x = T.argmax(py_x, axis=1)
+
+cost = T.mean(T.nnet.categorical_crossentropy(noise_py_x, Y))
+params = [w_h, w_h2, w_o]
+updates = RMSprop(cost, params, lr=0.001)
 
 train = theano.function(inputs=[X,Y], outputs=cost, updates=update, allow_input_downcast=True)
 predict = theano.function(inputs=[X], outputs=pred_y, allow_input_downcast=True)
@@ -35,59 +37,3 @@ print test_accuracy(TRAIN_SIZE, predict, train_img, train_res)
 
 endt = time.time()
 print endt - startt
-# D = (result, labels)
-# y = model()
-# x = T.dmatrix('x')
-
-# print w
-# N = 4
-# feats = 5
-# D = (rng.randn(N, feats), rng.randint(size=N, low=0, high=2))
-# x = T.dmatrix('x')
-# s = T.sum(1 / (1 + T.exp(-x)))
-# gs = T.grad(s, x)
-# print D[0], theano.function([x], gs)(D[0])
-
-
-
-# training_steps = 10000
-# print "Train: ", D[0]
-# print "Result: ", D[1]
-# # Declare Theano symbolic variables
-# x = T.matrix("x")
-# y = T.vector("y")
-# w = theano.shared(rng.randn(feats), name="w")
-# b = theano.shared(0., name="b")
-# print("Initial model:")
-# print(w.get_value())
-# print(b.get_value())
-
-# # Construct Theano expression graph
-# print "matrix: ", theano.function([x], T.dot(x, w) - b)(D[0])
-# print "matrix: ", theano.function([x], T.dot(x, w))(D[0])
-# p_1 = 1 / (1 + T.exp(-T.dot(x, w) - b))   # Probability that target = 1
-# prediction = p_1 > 0.5                    # The prediction thresholded
-# xent = -y * T.log(p_1) - (1-y) * T.log(1-p_1) # Cross-entropy loss function
-# cost = xent.mean() + 0.01 * (w ** 2).sum()# The cost to minimize
-# gw, gb = T.grad(cost, [w, b])             # Compute the gradient of the cost
-#                                           # (we shall return to this in a
-#                                           # following section of this tutorial)
-
-# # Compile
-# train = theano.function(
-#           inputs=[x,y],
-#           outputs=[prediction, xent],
-#           updates=((w, w - 0.1 * gw), (b, b - 0.1 * gb)))
-# predict = theano.function(inputs=[x], outputs=prediction)
-
-# # Train
-# for i in range(training_steps):
-#     pred, err = train(D[0], D[1])
-
-# print("Final model:")
-# print(w.get_value())
-# print(b.get_value())
-# print("target values for D:")
-# print(D[1])
-# print("prediction on D:")
-# print(predict(D[0]))
