03bb6dc065e0e0f511c24b654d18b3f43b41662c
chepiga1995 <ura1995ura@gmail.com>
2015-11-29 11:48:36 +0200

    test

--- test.py
+++ test.py
@@ -31,15 +31,11 @@ for x in range(TRAIN_CIRCLES):
 	for start, end in zip(range(0, len(train_img), BATCHES), range(BATCHES, len(train_img), BATCHES)):
 		cost = train(train_img[start:end], train_res[start:end])
 	if x % 10 == 0:
-		# print np.mean(np.argmax(test_res, axis=1) == predict(test_img))
-		# print np.mean(np.argmax(train_res, axis=1) == predict(train_img))
-		print test_accuracy(TEST_SIZE, predict, test_img, test_res)
-		print test_accuracy(TRAIN_SIZE, predict, train_img, train_res)
+		print np.mean(np.argmax(test_res, axis=1) == predict(test_img))
+		print np.mean(np.argmax(train_res, axis=1) == predict(train_img))
 
-# print np.mean(np.argmax(test_res, axis=1) == predict(test_img))
-# print np.mean(np.argmax(train_res, axis=1) == predict(train_img))
-print test_accuracy(TEST_SIZE, predict, test_img, test_res)
-print test_accuracy(TRAIN_SIZE, predict, train_img, train_res)
+print np.mean(np.argmax(test_res, axis=1) == predict(test_img))
+print np.mean(np.argmax(train_res, axis=1) == predict(train_img))
 
 endt = time.time()
 print endt - startt
