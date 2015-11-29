6cc5a7c4a8df74d98ce58fa396fd7ac48b66b273
chepiga1995 <ura1995ura@gmail.com>
2015-11-29 11:23:53 +0200

    test

--- test.py
+++ test.py
@@ -2,9 +2,7 @@ import time
 startt = time.time()
 from read_fromfile import *
 from libs_for_train import *
-from load import mnist
 
-train_img, test_img, train_res, test_res = mnist(onehot=True)
 
 SPEED = 0.05
 TRAIN_CIRCLES = 130
@@ -28,24 +26,20 @@ updates = RMSprop(cost, params, lr=0.001)
 train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
 predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
 
-# for i in range(100):
-#     for start, end in zip(range(0, len(train_img), 128), range(128, len(train_img), 128)):
-#         cost = train(train_img[start:end], train_res[start:end])
-#     print np.mean(np.argmax(test_res, axis=1) == predict(test_img))
 
 for x in range(TRAIN_CIRCLES):
 	for start, end in zip(range(0, len(train_img), BATCHES), range(BATCHES, len(train_img), BATCHES)):
 		cost = train(train_img[start:end], train_res[start:end])
 	if x % 10 == 0:
-		print np.mean(np.argmax(test_res, axis=1) == predict(test_img))
-		print np.mean(np.argmax(train_res, axis=1) == predict(train_img))
-		# print test_accuracy(TEST_SIZE, predict, test_img, test_res)
-		# print test_accuracy(TRAIN_SIZE, predict, train_img, train_res)
+		# print np.mean(np.argmax(test_res, axis=1) == predict(test_img))
+		# print np.mean(np.argmax(train_res, axis=1) == predict(train_img))
+		print test_accuracy(TEST_SIZE, predict, test_img, test_res)
+		print test_accuracy(TRAIN_SIZE, predict, train_img, train_res)
 
-print np.mean(np.argmax(test_res, axis=1) == predict(test_img))
-print np.mean(np.argmax(train_res, axis=1) == predict(train_img))
-# print test_accuracy(TEST_SIZE, predict, test_img, test_res)
-# print test_accuracy(TRAIN_SIZE, predict, train_img, train_res)
+# print np.mean(np.argmax(test_res, axis=1) == predict(test_img))
+# print np.mean(np.argmax(train_res, axis=1) == predict(train_img))
+print test_accuracy(TEST_SIZE, predict, test_img, test_res)
+print test_accuracy(TRAIN_SIZE, predict, train_img, train_res)
 
 endt = time.time()
 print endt - startt
